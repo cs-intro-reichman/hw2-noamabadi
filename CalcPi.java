f061073841964b59cb59ad2419b5f6c139449bd4
noamabadi <noam.abadi@post.runi.ac.il>
2024-11-18 19:04:29 +0200

    n

--- CalcPi.java
+++ CalcPi.java
@@ -1,27 +1,17 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    int calc = Integer.parseInt(args[0]);
-		double divider = 3.0;
-		double sum = 1.0; 
-		int counter = 1;
-		boolean possitiveOrNegative = false;
+	int num = Integer.parseInt(args[0]);
+	int i = 0;
+	double sum = 0.0;
 
-		while (counter < calc) {
-
-			if (possitiveOrNegative == false){
-
-			sum = sum - (1.0 / divider); 
-			possitiveOrNegative = true;	}
-			 else { 
-				sum = sum + (1.0 / divider);
-				possitiveOrNegative = false;
-				}
-				
-			counter++;
-			divider = divider + 2.0;
-		}
-		System.out.println("pi according to java: " + Math.PI);
-		System.out.println("pi, approximated:     " + sum * 4.0);
+	for (i = 0; i < num; i++){
+	double term = Math.pow(-1, i) / (2*i + 1);
+	sum = sum + term;
 	}
-}
+
+	double pi = sum * 4;
+	System.out.println("pi according to java: " + Math.PI);
+	System.out.println("pi, approximated:     " + pi); 
+	} }
+
